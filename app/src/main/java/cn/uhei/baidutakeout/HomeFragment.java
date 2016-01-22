1c79aa8ca4c0d93800fbea5952bfe623a675e843
mytime <cmba@qq.com>
2016-01-22 22:23:35 +0800

    子菜单

--- app/src/main/java/cn/uhei/baidutakeout/HomeFragment.java
+++ app/src/main/java/cn/uhei/baidutakeout/HomeFragment.java
@@ -5,16 +5,53 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    private ListView lvHome;
+//    private List<News> newsList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_home,container,false);
+
+//        lvHome = (ListView) v.findViewById(R.id.lvHome);
+//        lvHome.setAdapter(new myAdapter());
+
+
+        return v;
+
+
     }
 
 
+//    private class myAdapter extends BaseAdapter {
+//        @Override
+//        public int getCount() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public Object getItem(int position) {
+//            return null;
+//        }
+//
+//        @Override
+//        public long getItemId(int position) {
+//            return 0;
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            return null;
+//        }
+//    }
 }
